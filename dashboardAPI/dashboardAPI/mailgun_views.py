da29c1c854b2dd507cdf2fb938d655262205aa43
Poly1581 <bspaulsen@dons.usfca.edu>
2025-07-18 14:19:55 -0700

    Added data filtering for mailgun PUT views and added documentation for all mailgun views

--- dashboardAPI/dashboardAPI/mailgun_views.py
+++ dashboardAPI/dashboardAPI/mailgun_views.py
@@ -12,6 +12,10 @@ AUTH = ('api', f"{API_KEY}")
 
 @api_view(["GET"])
 def get_mailgun_queue_status(request, **kwargs):
+    '''
+        Endpoint to access mailgun messages queue status.
+        See: https://documentation.mailgun.com/docs/mailgun/api-reference/openapi-final/messages/get-v3-domains--name--sending-queues
+    '''
     URI = f"{BASE_URI}/v3/domains/{API_NAME}/sending_queues"
     try:
         response = requests.get(URI, auth = AUTH)
@@ -28,9 +32,14 @@ def get_mailgun_queue_status(request, **kwargs):
 
 @api_view(["PUT"])
 def get_mailgun_account_metrics(request, **kwargs):
+    '''
+        Endpoint to access mailgun account metrics.
+        See: https://documentation.mailgun.com/docs/mailgun/api-reference/openapi-final/metrics/post-v1-analytics-metrics
+    '''
     URI = f"{BASE_URI}/v1/analytics/metrics"
+    filtered_data = {key: value for key, value in request.data.items() if key in ("start", "end", "resolution", "duration", "dimensions", "metrics", "filter")}
     try:
-        response = requests.post(URI, auth = AUTH)
+        response = requests.post(URI, auth = AUTH, json = filtered_data, headers={"Content-Type": "application/json"})
         response.raise_for_status() # Raise HTTPError for bad responses (4xx or 5xx)
         return HttpResponse(json.dumps(response.json()), content_type="application/json")
     except requests.exceptions.RequestException as e:
@@ -44,9 +53,14 @@ def get_mailgun_account_metrics(request, **kwargs):
 
 @api_view(["PUT"])
 def get_mailgun_account_usage_metrics(request, **kwargs):
+    '''
+        Endpoint to access mailgun account usage metrics
+        See: https://documentation.mailgun.com/docs/mailgun/api-reference/openapi-final/metrics/post-v1-analytics-usage-metrics
+    '''
     URI = f"{BASE_URI}/v1/analytics/usage/metrics"
+    filtered_data = {key: value for key, value in request.data.items() if key in ("start", "end", "resolution", "duration", "dimensions", "metrics", "filter")}
     try:
-        response = requests.post(URI, auth = AUTH)
+        response = requests.post(URI, auth = AUTH, json = filtered_data, headers={"Content-Type": "application/json"})
         response.raise_for_status() # Raise HTTPError for bad responses (4xx or 5xx)
         return HttpResponse(json.dumps(response.json()), content_type="application/json")
     except requests.exceptions.RequestException as e:
@@ -57,3 +71,5 @@ def get_mailgun_account_usage_metrics(request, **kwargs):
         error_message = f"An unexpected error occurred in get_mailgun_account_usage_metrics: {error}"
         print(error_message)
         return HttpResponseBadRequest(error_message)
+
+
