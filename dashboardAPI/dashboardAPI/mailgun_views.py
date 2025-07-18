4352074d41afab8c8b3bab277c74012e86e48efd
Poly1581 <bspaulsen@dons.usfca.edu>
2025-07-18 16:28:22 -0700

    Added more mailgun api endpoints

--- dashboardAPI/dashboardAPI/mailgun_views.py
+++ dashboardAPI/dashboardAPI/mailgun_views.py
@@ -10,11 +10,22 @@ API_KEY = os.environ.get("MAILGUN_API_KEY")
 BASE_URI = "https://api.mailgun.net"
 AUTH = ('api', f"{API_KEY}")
 
+request_params = {
+    "get_account_metrics": ("start", "end", "resolution", "duration", "dimensions", "metrics", "filter"),
+    "get_account_usage_metrics": ("start", "end", "resolution", "duration", "dimensions", "metrics", "filter"),
+    "get_logs": ("start", "end", "events", "metric_events", "filter", "include_subaccounts", "include_totals", "pagination"),
+    "get_stat_totals": ("start", "end", "resolution", "duration", "event")
+}
+
+def filter_data(data, view):
+    return {key: value for key, value in data.items() if key in request_params[view]}
+
+
 @api_view(["GET"])
-def get_mailgun_queue_status(request, **kwargs):
+def get_queue_status(request, **kwargs):
     '''
         Endpoint to access mailgun messages queue status.
-        See: https://documentation.mailgun.com/docs/mailgun/api-reference/openapi-final/messages/get-v3-domains--name--sending-queues
+        See https://documentation.mailgun.com/docs/mailgun/api-reference/openapi-final/messages/get-v3-domains--name--sending-queues
     '''
     URI = f"{BASE_URI}/v3/domains/{API_NAME}/sending_queues"
     try:
@@ -31,13 +42,13 @@ def get_mailgun_queue_status(request, **kwargs):
         return HttpResponseBadRequest(error_message)
 
 @api_view(["PUT"])
-def get_mailgun_account_metrics(request, **kwargs):
+def get_account_metrics(request, **kwargs):
     '''
         Endpoint to access mailgun account metrics.
         See: https://documentation.mailgun.com/docs/mailgun/api-reference/openapi-final/metrics/post-v1-analytics-metrics
     '''
     URI = f"{BASE_URI}/v1/analytics/metrics"
-    filtered_data = {key: value for key, value in request.data.items() if key in ("start", "end", "resolution", "duration", "dimensions", "metrics", "filter")}
+    filtered_data = filter_data(request.data, "get_account_metrics")
     try:
         response = requests.post(URI, auth = AUTH, json = filtered_data, headers={"Content-Type": "application/json"})
         response.raise_for_status() # Raise HTTPError for bad responses (4xx or 5xx)
@@ -52,13 +63,13 @@ def get_mailgun_account_metrics(request, **kwargs):
         return HttpResponseBadRequest(error_message)
 
 @api_view(["PUT"])
-def get_mailgun_account_usage_metrics(request, **kwargs):
+def get_account_usage_metrics(request, **kwargs):
     '''
         Endpoint to access mailgun account usage metrics
         See: https://documentation.mailgun.com/docs/mailgun/api-reference/openapi-final/metrics/post-v1-analytics-usage-metrics
     '''
     URI = f"{BASE_URI}/v1/analytics/usage/metrics"
-    filtered_data = {key: value for key, value in request.data.items() if key in ("start", "end", "resolution", "duration", "dimensions", "metrics", "filter")}
+    filtered_data = filter_data(request.data, "get_account_usage_metrics")
     try:
         response = requests.post(URI, auth = AUTH, json = filtered_data, headers={"Content-Type": "application/json"})
         response.raise_for_status() # Raise HTTPError for bad responses (4xx or 5xx)
@@ -72,4 +83,65 @@ def get_mailgun_account_usage_metrics(request, **kwargs):
         print(error_message)
         return HttpResponseBadRequest(error_message)
 
+@api_view(["PUT"])
+def get_logs(request, **kwargs):
+    '''
+        Endpoint to access mailgun logs
+        See: https://documentation.mailgun.com/docs/mailgun/api-reference/openapi-final/logs/post-v1-analytics-logs
+    '''
+    URI = f"{BASE_URI}/v1/analytics/logs"
+    filtered_data = filter_data(request.data, "get_logs")
+    try:
+        response = requests.post(URI, auth = AUTH, json = filtered_data, headers={"Content-Type": "application/json"})
+        response.raise_for_status() # Raise HTTPError for bad responses (4xx or 5xx)
+        return HttpResponse(json.dumps(response.json()), content_type="application/json")
+    except requests.exceptions.RequestException as e:
+        error_message = f"Error fetching mailgun account usage metrics: {e}"
+        print(error_message)
+        return HttpResponseBadRequest(error_message)
+    except Exception as error:
+        error_message = f"An unexpected error occurred in get_mailgun_account_usage_metrics: {error}"
+        print(error_message)
+        return HttpResponseBadRequest(error_message)
 
+@api_view(["PUT"])
+def get_stat_totals(request, **kwargs):
+    '''
+        Endpoint to access mailgun stats
+        See: https://documentation.mailgun.com/docs/mailgun/api-reference/openapi-final/stats/get-v3-stats-total
+    '''
+    URI = f"{BASE_URI}/v3/stats/total"
+    filtered_data = filter_data(request.data, "get_stat_totals")
+    try:
+        response = requests.get(URI, auth = AUTH, params=filtered_data)
+        response.raise_for_status() # Raise HTTPError for bad responses (4xx or 5xx)
+        return HttpResponse(json.dumps(response.json()), content_type="application/json")
+    except requests.exceptions.RequestException as e:
+        error_message = f"Error fetching mailgun account usage metrics: {e}"
+        print(error_message)
+        return HttpResponseBadRequest(error_message)
+    except Exception as error:
+        error_message = f"An unexpected error occurred in get_mailgun_account_usage_metrics: {error}"
+        print(error_message)
+        return HttpResponseBadRequest(error_message)
+
+@api_view(["PUT"])
+def get_filtered_grouped_stats(request, **kwargs):
+    '''
+        Endpoint to access mailgun filtered/grouped stats
+        See https://documentation.mailgun.com/docs/mailgun/api-reference/openapi-final/stats/get-v3-stats-filter
+    '''
+    URI = f"{BASE_URI}/v3/stats/filter"
+    filtered_data = filter_data(request.data, "get_filtered_grouped_stats")
+    try:
+        response = requests.get(URI, auth = AUTH, params=filtered_data)
+        response.raise_for_status() # Raise HTTPError for bad responses (4xx or 5xx)
+        return HttpResponse(json.dumps(response.json()), content_type="application/json")
+    except requests.exceptions.RequestException as e:
+        error_message = f"Error fetching mailgun account usage metrics: {e}"
+        print(error_message)
+        return HttpResponseBadRequest(error_message)
+    except Exception as error:
+        error_message = f"An unexpected error occurred in get_mailgun_account_usage_metrics: {error}"
+        print(error_message)
+        return HttpResponseBadRequest(error_message)
--- dashboardAPI/dashboardAPI/urls.py
+++ dashboardAPI/dashboardAPI/urls.py
@@ -9,7 +9,7 @@ Function views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based vi ews
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('',  Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
@@ -33,7 +33,10 @@ urlpatterns = [
     path("api/hubspot/integration-status/", integration.get_hubspot_integration_status, name="get hubspot integration status"),
 
     # Mailgun API endpoints
-    path("api/mailgun/queue-status/", mailgun.get_mailgun_queue_status, name="get mailgun whitelist"),
-    path("api/mailgun/account-metrics/", mailgun.get_mailgun_account_metrics, name="get mailgun account metrics"),
-    path("api/mailgun/account-usage-metrics/", mailgun.get_mailgun_account_usage_metrics, name="get"),
+    path("api/mailgun/queue-status/", mailgun.get_queue_status, name="get mailgun whitelist"),
+    path("api/mailgun/account-metrics/", mailgun.get_account_metrics, name="get mailgun account metrics"),
+    path("api/mailgun/account-usage-metrics/", mailgun.get_account_usage_metrics, name="get mailgun account usage metrics"),
+    path("api/mailgun/logs/", mailgun.get_logs, name="get mailgun logs"),
+    path("api/mailgun/stats/totals", mailgun.get_stat_totals, name = "get mailgun stat totals"),
+    path("api/mailgun/stats/filter", mailgun.get_filtered_grouped_stats, name = "get filtered mailgun stats"),
 ]
