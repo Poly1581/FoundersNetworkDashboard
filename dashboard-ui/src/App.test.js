fab6cb91c6fc7dbd527c9654bbab5af14df4491a
Poly1581 <bspaulsen@dons.usfca.edu>
2025-07-24 21:25:30 -0700

    Tests are running now, need to figure out network mocking... yay!

--- dashboard-ui/src/App.test.js
+++ dashboard-ui/src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { waitFor, render, screen } from '@testing-library/react';
 import React from "react";
 import App from './App';
 import { QueryClientProvider } from '@tanstack/react-query';
@@ -6,28 +6,24 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import CssBaseline from '@mui/material/CssBaseline';
 import { queryClient } from './queryClient';
 
-test('Loads sentry properly', async () => {
-	try {
-		render(
-			<QueryClientProvider client={queryClient}>
-				<CssBaseline />
-				<App />
-				<ReactQueryDevtools initialIsOpen={false} />
-			</QueryClientProvider>
-		);
-	} catch (error) {
-		/*
-			Rendering causes AggregateError composed of 3 of the following:
-				ReferenceError: ResizeObserver is not defined
-				at /dashboard-frontend/node_modules/recharts/lib/component/ResponsiveContainer.js:80:20
-			This is likely due to using recharts, hopefully this will be fixed by switching to mui charts
-			If not, we will have to mock ResizeObserver, which won't be too bad (I hope)
+global.ResizeObserver = jest.fn().mockImplementation(() => ({
+	observe: jest.fn(),
+	unobserve: jest.fn(),
+	disconnect: jest.fn(),
+}));
 
-			Uncomment the following line to log the error when testing:
-		*/
-		
-		// console.log(error);
-	}
-	expect(screen.queryByText("Loading Sentry Data")).toBe(null);
-	expect(screen.queryByText("Live Data")).not.toBe(null);
+test('Loads sentry properly', async () => {
+	render(
+		<QueryClientProvider client={queryClient}>
+			<CssBaseline />
+			<App />
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	);
+	await waitFor(() => {
+		expect(screen.queryAllByText("Sentry")).not.toBe([]);
+	});
+	await waitFor(() => {
+		expect(screen.queryByText("Loading Sentry Data...")).toBe(null);
+	});
 });
